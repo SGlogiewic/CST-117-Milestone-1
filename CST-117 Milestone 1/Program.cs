d2148470943aeece7ab06aa6b97a217bd858b2f1
StephenGlogiewicz <sglogiewic@my.gcu.edu>
2020-11-07 12:28:28 -0700

    Add project files.

---
+++ CST-117 Milestone 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CST_117_Milestone_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int plantStock = 1;
+            int seedStock = 2;
+            int toolStock = 3;
+            int soilStock = 4;
+            int fertilizerStock = 5;
+            Console.WriteLine("Gardening Supply Inventory");
+            Console.WriteLine("Items in Stock: ");
+            Console.WriteLine("Live Plants" + " = " + plantStock);
+            Console.WriteLine("Seeds" + " = " + seedStock);
+            Console.WriteLine("Tools" + " = " + toolStock);
+            Console.WriteLine("Soil" + " = " + soilStock);
+            Console.WriteLine("Fertilizer" +  " = " + fertilizerStock);
+
+        }
+    }
+}
